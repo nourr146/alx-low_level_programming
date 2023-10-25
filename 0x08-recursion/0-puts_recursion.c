8ed8051375a1e37417dc5a2a42a0f199201b95d1
nourr146 <nourb146@gmail.com>
2023-10-25 12:00:52 -0700

    0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,19 @@
-#include "main.h"
+
 /**
- * _puts_recursion - print a string
- * @s:string
- * Return:void
- */
+ * _puts_recursion - a function that prints a new line
+ *
+ * @s: the string
+ *
+ * Return: nothing
+*/
+
 void _puts_recursion(char *s)
 {
-if (*s)
-{
-	_putchar(*s);
-	_puts_recursion(s + 1);
-}
-else
-{
-	_putchar('\n');
-}
-
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
 }
