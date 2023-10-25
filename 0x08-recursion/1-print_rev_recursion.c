96f8f1952804b7a985ace8e8cba262df9344bc49
nourr146 <nourb146@gmail.com>
2023-10-25 12:54:43 -0700

    1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include main.h"
 
 /**
  * _print_rev_recursion - prints a string in reverse
@@ -10,7 +10,7 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s > '\0')
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
